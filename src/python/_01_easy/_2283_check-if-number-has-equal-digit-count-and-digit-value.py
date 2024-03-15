f8138cc060341810267b519ba99614eaff1bd195
Maria Pugacheva <marie.pugacheva@gmail.com>
2024-03-15 15:18:42 -0700

    Add a solution file for #2283

---
+++ src/python/_01_easy/_2283_check-if-number-has-equal-digit-count-and-digit-value.py
@@ -0,0 +1,57 @@
+import doctest
+
+
+# ---------------------------------------------------------------------
+# Approach 1: Brute Force. Time: O(n)                               ***
+# ---------------------------------------------------------------------
+def solution_one(num: str) -> bool:
+    """Return True if for every index i in the range 0 <= i < n, the
+    digit i occurs num[i] times in num, otherwise return False.
+
+    Preconditions:
+        1 <= len(num) <= 10
+
+    Examples:
+        >>> solution_one('1210')
+        True
+        >>> solution_one('030')
+        False
+        >>> solution_one('121010908')
+        False
+    """
+    d = {}
+    for ch in num:
+        d[int(ch)] = d.get(int(ch), 0) + 1
+    for i in range(len(num)):
+        if (i in d and d[i] != int(num[i])) or \
+                (i not in d and int(num[i]) != 0):
+            return False
+    return True
+
+
+# ---------------------------------------------------------------------
+# Approach 2: Autobiographical Numbers. Time: O(n)                  ^**
+# ---------------------------------------------------------------------
+def solution_two(num: str) -> bool:
+    """Return True if for every index i in the range 0 <= i < n, the
+    digit i occurs num[i] times in num, otherwise return False.
+
+    Preconditions:
+        1 <= len(num) <= 10
+
+    Examples:
+        >>> solution_two('1210')
+        True
+        >>> solution_two('030')
+        False
+        >>> solution_two('121010908')
+        False
+    """
+    if num in {'1210', '2020', '21200', '3211000', '42101000',
+               '521001000', '6210001000'}:
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    doctest.testmod()
