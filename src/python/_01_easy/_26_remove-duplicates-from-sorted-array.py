0c56e356776091167808b808bede8950e5a59ea0
Maria Pugacheva <marie.pugacheva@gmail.com>
2024-06-01 16:24:31 -0700

    Update #26

--- src/python/_01_easy/_26_remove-duplicates-from-sorted-array.py
+++ src/python/_01_easy/_26_remove-duplicates-from-sorted-array.py
@@ -3,34 +3,28 @@ from typing import List
 
 
 # ---------------------------------------------------------------------
-# Approach 1: Two Pointers. Time: O(n). Space: O(1).                ***
+# Approach 1: Two Pointers. Time: O(n). Space: O(1)                 ***
 # ---------------------------------------------------------------------
-# 1. Create a slow pointer named i.
-# 2. Use a fast pointer j to iterate over the given list and look for
-#    distinct elements.
-# 3. Once j reaches the end of the given list, return count + 1.
-# ---------------------------------------------------------------------
-def solution_one(nums: List[int]) -> int:
+def solution(nums: List[int]) -> int:
     """Remove the duplicates in-place and return the new length of nums.
 
     Preconditions:
         nums is sorted
 
     Examples:
-        >>> solution_one([1, 2])
+        >>> solution([1, 2])
         2
-        >>> solution_one([1, 1, 2, 3])
+        >>> solution([1, 1, 2, 3])
         3
-        >>> solution_one([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
+        >>> solution([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
         5
     """
-    count = 1
     i = 0
-    for j in range(1, len(nums)):
+    for j in range(len(nums)):
         if nums[i] != nums[j]:
-            count += 1
-        i += 1
-    return count
+            nums[i + 1] = nums[j]
+            i += 1
+    return i + 1
 
 
 if __name__ == '__main__':
