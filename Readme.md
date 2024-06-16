04c168f9fca090cea96cf142668d1e0d8c4eabbc
Maria Pugacheva <marie.pugacheva@gmail.com>
2024-06-16 11:01:10 -0700

    Update #49

--- Readme.md
+++ Readme.md
@@ -17,7 +17,7 @@ LeetCode
 |27|[Remove Element](https://leetcode.com/problems/remove-element/description/)|[Python](src/python/_01_easy/_27_remove-element.py)|Easy|2|Y|
 |28|[Find the Index of the First Occurrence in a String](https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/)|[Python](src/python/_01_easy/_28_find-the-index-of-the-first-occurrence-in-a-string.py)|Easy|1|YY|
 |35|[Search Insert Position](https://leetcode.com/problems/search-insert-position/)|[Python](src/python/_01_easy/_35_search-insert-position.py)|Easy|1|
-|49|[Group Anagrams](https://leetcode.com/problems/group-anagrams/description/)|[Python](src/python/_02_medium/_49_group-anagrams.py)|Medium|1|
+|49|[Group Anagrams](https://leetcode.com/problems/group-anagrams/description/)|[Python](src/python/_02_medium/_49_group-anagrams.py)|Medium|2|Y|
 |56|[Merge Intervals](https://leetcode.com/problems/merge-intervals/)|[Python](src/python/_02_medium/_56_merge_intervals.py)|Medium|1|
 |58|[Length of Last Word](https://leetcode.com/problems/length-of-last-word/)|[Python](src/python/_01_easy/_58_length-of-last-word.py)|Easy|2|Y|
 |80|[Remove Duplicates from Sorted Array II](https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/)|[Python](src/python/_02_medium/_80_remove-duplicates-from-sorted-array-II.py)|Medium|2|Y|
--- src/python/_02_medium/_49_group-anagrams.py
+++ src/python/_02_medium/_49_group-anagrams.py
@@ -1,31 +1,67 @@
 import doctest
-from collections import defaultdict
 from typing import List
 
 
 # ---------------------------------------------------------------------
-# Approach 1: Sorting + Dictionary                                  ***
+# Approach 1: Sorting. T: O(n k log k). S: O(nk)                    ***
+# ---------------------------------------------------------------------
+# Complexity Analysis: k is the maximum length of a string in strs
 # ---------------------------------------------------------------------
 def solution_one(strs: List[str]) -> List[List[str]]:
     """Given an array of strings strs, group the anagrams together.
 
-       Preconditions:
-           1 <= len(strs) <= 10^4
-           0 <= len(strs[i]) <= 100
-           strs[i] consists of lowercase English letters
+    Preconditions:
+        1 <= len(strs) <= 10^4
+        0 <= len(strs[i]) <= 100
+        strs[i] consists of lowercase English letters
 
-       Examples:
-           >>> solution_one(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])
-           [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
-           >>> solution_one([''])
-           [['']]
-           >>> solution_one([''])
-           [['']]
-       """
-    groups = defaultdict(list)
+    Examples:
+        >>> solution_one(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])
+        [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+        >>> solution_one([''])
+        [['']]
+        >>> solution_one([''])
+        [['']]
+    """
+    groups = {}
     for s in strs:
-        a = ''.join(sorted(s))
-        groups[a].append(s)
+        t = ''.join(sorted(s))
+        if t not in groups:
+            groups[t] = []
+        groups[t].append(s)
+    return list(groups.values())
+
+
+# ---------------------------------------------------------------------
+# Approach 2: Categorize by Count. T: O(nk). S: O(nk)               ^**
+# ---------------------------------------------------------------------
+# Complexity Analysis: k is the maximum length of a string in strs
+# ---------------------------------------------------------------------
+def solution_two(strs: List[str]) -> List[List[str]]:
+    """Given an array of strings strs, group the anagrams together.
+
+    Preconditions:
+        1 <= len(strs) <= 10^4
+        0 <= len(strs[i]) <= 100
+        strs[i] consists of lowercase English letters
+
+    Examples:
+        >>> solution_two(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])
+        [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+        >>> solution_two([''])
+        [['']]
+        >>> solution_two([''])
+        [['']]
+    """
+    groups = {}
+    for s in strs:
+        cnt = [0] * 26
+        for ch in s:
+            cnt[ord(ch) - 97] += 1
+        t = tuple(cnt)
+        if t not in groups:
+            groups[t] = []
+        groups[t].append(s)
     return list(groups.values())
 
 
