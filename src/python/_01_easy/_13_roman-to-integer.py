c5dcb7b879725b689edeeb0d2e7fa0f6b42629e6
Maria Pugacheva <marie.pugacheva@gmail.com>
2024-07-11 13:29:33 -0700

    Update #13

--- src/python/_01_easy/_13_roman-to-integer.py
+++ src/python/_01_easy/_13_roman-to-integer.py
@@ -16,15 +16,13 @@ def solution(s: str) -> int:
         1994
     """
     hm = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    res = i = 0
-    n = len(s)
-    while i < n:
-        if i < n - 1 and hm[s[i]] < hm[s[i+1]]:
-            res -= hm[s[i]]
-            i += 1
-        res += hm[s[i]]
-        i += 1
-    return res
+    n = hm[s[-1]]
+    for i in range(len(s) - 1):
+        if hm[s[i]] < hm[s[i + 1]]:
+            n -= hm[s[i]]
+        else:
+            n += hm[s[i]]
+    return n
 
 
 if __name__ == '__main__':
