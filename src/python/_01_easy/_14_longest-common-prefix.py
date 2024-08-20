b0959935b7a1bbc15db8c41fde960503a1fd31db
Maria Pugacheva <marie.pugacheva@gmail.com>
2024-08-19 19:35:38 -0700

    Update #14

--- src/python/_01_easy/_14_longest-common-prefix.py
+++ src/python/_01_easy/_14_longest-common-prefix.py
@@ -3,45 +3,18 @@ from typing import List
 
 
 # ---------------------------------------------------------------------
-# Approach 1: Scanning I. Time: O(n * m), m == len(shortest)        ***
+# Approach 1: Scanning. Time: O(n * m), m == len(shortest)          ***
 # ---------------------------------------------------------------------
-def solution_one(words: List[str]) -> str:
+def solution(words: List[str]) -> str:
     """Find the longest common prefix string amongst an array of
     strings words. If there is no common prefix, return an empty string.
 
     Examples:
-        >>> solution_one(['dog', 'racecar', 'car'])
+        >>> solution(['dog', 'racecar', 'car'])
         ''
-        >>> solution_one(['a'])
+        >>> solution(['a'])
         'a'
-        >>> solution_one(['flower', 'flow', 'flight'])
-        'fl'
-    """
-    prefix = []
-    shortest = min([len(w) for w in words])
-    diff = False
-    for i in range(shortest):
-        for j in range(len(words)):
-            if words[j][i] != words[0][i]:
-                diff = True
-        if not diff:
-            prefix.append(words[0][i])
-    return ''.join(prefix)
-
-
-# ---------------------------------------------------------------------
-# Approach 2: Scanning II. Time: O(n * m), m == len(shortest)       ***
-# ---------------------------------------------------------------------
-def solution_two(words: List[str]) -> str:
-    """Find the longest common prefix string amongst an array of
-    strings words. If there is no common prefix, return an empty string.
-
-    Examples:
-        >>> solution_two(['dog', 'racecar', 'car'])
-        ''
-        >>> solution_two(['a'])
-        'a'
-        >>> solution_two(['flower', 'flow', 'flight'])
+        >>> solution(['flower', 'flow', 'flight'])
         'fl'
     """
     for i in range(len(words[0])):
