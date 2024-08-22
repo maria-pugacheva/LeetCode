7367c1b399c8c6f6c62441edfe8b37a5c4b02e9e
Maria Pugacheva <marie.pugacheva@gmail.com>
2024-08-22 14:29:16 -0700

    Update #88

--- src/python/_01_easy/_88_merge-sorted-array.py
+++ src/python/_01_easy/_88_merge-sorted-array.py
@@ -3,21 +3,21 @@ from typing import List
 
 
 # ---------------------------------------------------------------------
-# Approach 1: Sorting. Time: O(n log n)                             !**
+# Approach 1: Sorting. Time: O((n + m) log(n + m))                  !**
 # ---------------------------------------------------------------------
 def solution_one(nums1: List[int], m: int, nums2: List[int], n: int) \
         -> List[int]:
     """Merge nums1 and nums2 into a single array sorted in ASC order.
 
-       Examples:
-           >>> solution_one([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
-           [1, 2, 2, 3, 5, 6]
-           >>> solution_one([1], 1, [], 0)
-           [1]
-           >>> solution_one([0], 0, [1], 1)
-           [1]
-           >>> solution_one([2, 0], 1, [1], 1)
-           [1, 2]
+    Examples:
+        >>> solution_one([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+        [1, 2, 2, 3, 5, 6]
+        >>> solution_one([1], 1, [], 0)
+        [1]
+        >>> solution_one([0], 0, [1], 1)
+        [1]
+        >>> solution_one([2, 0], 1, [1], 1)
+        [1, 2]
     """
     for i in range(n):
         nums1[m] = nums2[i]
@@ -27,21 +27,21 @@ def solution_one(nums1: List[int], m: int, nums2: List[int], n: int) \
 
 
 # ---------------------------------------------------------------------
-# Approach 2: Two Pointers. Time: O(n)                              !**
+# Approach 2: Two Pointers. Time: O(n + m)                          !**
 # ---------------------------------------------------------------------
 def solution_two(nums1: List[int], m: int, nums2: List[int], n: int) \
         -> List[int]:
     """Merge nums1 and nums2 into a single array sorted in ASC order.
 
-       Examples:
-           >>> solution_two([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
-           [1, 2, 2, 3, 5, 6]
-           >>> solution_two([1], 1, [], 0)
-           [1]
-           >>> solution_two([0], 0, [1], 1)
-           [1]
-           >>> solution_two([2, 0], 1, [1], 1)
-           [1, 2]
+    Examples:
+        >>> solution_two([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+        [1, 2, 2, 3, 5, 6]
+        >>> solution_two([1], 1, [], 0)
+        [1]
+        >>> solution_two([0], 0, [1], 1)
+        [1]
+        >>> solution_two([2, 0], 1, [1], 1)
+        [1, 2]
     """
     m -= 1
     n -= 1
