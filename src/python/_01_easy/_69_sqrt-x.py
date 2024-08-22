3365953ed61b794f2d496d2a2417da2dfb442855
Maria Pugacheva <marie.pugacheva@gmail.com>
2024-08-21 20:15:46 -0700

    Add another solution for #69

--- src/python/_01_easy/_69_sqrt-x.py
+++ src/python/_01_easy/_69_sqrt-x.py
@@ -24,18 +24,53 @@ def solution_one(x: int) -> int:
         >>> solution_one(12)
         3
     """
-    if x * x == x:
+    if x < 2:
         return x
-    i, j = 1, x // 2
+    i, j = 2, x // 2
     while i <= j:
-        mid = i + (j - i) // 2
-        if mid * mid == x:
+        mid = (i + j) // 2
+        if mid ** 2 == x:
             return mid
-        elif mid * mid < x:
+        elif mid ** 2 < x:
             i = mid + 1
         else:
             j = mid - 1
     return j
+
+
+# ---------------------------------------------------------------------
+# Approach 2: Newton's Method. Time: O(log n)                         !
+# ---------------------------------------------------------------------
+def solution_two(x: int) -> int:
+    """Given a non-negative integer x, return the square root of x
+    rounded down to the nearest integer.
+
+    Examples:
+        >>> solution_two(0)
+        0
+        >>> solution_two(1)
+        1
+        >>> solution_two(3)
+        1
+        >>> solution_two(4)
+        2
+        >>> solution_two(8)
+        2
+        >>> solution_two(10)
+        3
+        >>> solution_two(12)
+        3
+    """
+    if x < 2:
+        return x
+
+    x0 = x
+    x1 = (x0 + x / x0) / 2
+    while abs(x0 - x1) >= 1:
+        x0 = x1
+        x1 = (x0 + float(x) / x0) / 2
+
+    return int(x1)
 
 
 if __name__ == '__main__':
