2fa7967dbff26565f19862a2623a4ee889bd91cd
Maria Pugacheva <marie.pugacheva@gmail.com>
2024-08-24 14:05:21 -0700

    Update #11

--- src/python/_02_medium/_11_container-with-most-water.py
+++ src/python/_02_medium/_11_container-with-most-water.py
@@ -3,26 +3,50 @@ from typing import List
 
 
 # ---------------------------------------------------------------------
-# Approach 1: Two Pointers. Time: O(n)                              ***
+# Approach 1: Brute Force. Time: O(n^2)                             ***
 # ---------------------------------------------------------------------
 def solution_one(nums: List[int]) -> int:
     """Return the maximum amount of water a container can store.
 
-       Preconditions:
-           n == len(nums)
-           2 <= n <= 10^5
-           0 <= nums[i] <= 10^4
+    Preconditions:
+        n == len(nums)
+        2 <= n <= 10^5
+        0 <= nums[i] <= 10^4
 
-       Examples:
-           >>> solution_one([1, 1])
-           1
-           >>> solution_one([1, 8, 6, 2, 5, 4, 8, 3, 7])
-           49
-       """
-    res = i = 0
-    j = len(nums) - 1
+    Examples:
+        >>> solution_one([1, 1])
+        1
+        >>> solution_one([1, 8, 6, 2, 5, 4, 8, 3, 7])
+        49
+    """
+    res = 0
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            res = max(res, min(nums[i], nums[j]) * (j - i))
+    return res
+
+
+# ---------------------------------------------------------------------
+# Approach 1: Two Pointers. Time: O(n)                              ***
+# ---------------------------------------------------------------------
+def solution_two(nums: List[int]) -> int:
+    """Return the maximum amount of water a container can store.
+
+    Preconditions:
+        n == len(nums)
+        2 <= n <= 10^5
+        0 <= nums[i] <= 10^4
+
+    Examples:
+        >>> solution_two([1, 1])
+        1
+        >>> solution_two([1, 8, 6, 2, 5, 4, 8, 3, 7])
+        49
+    """
+    res = 0
+    i, j = 0, len(nums) - 1
     while i < j:
-        res = max(res, (j - i) * min(nums[i], nums[j]))
+        res = max(res, min(nums[i], nums[j]) * (j - i))
         if nums[i] < nums[j]:
             i += 1
         else:
