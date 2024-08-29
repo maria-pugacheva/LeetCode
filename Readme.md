c3a00569fa0d42dccdc9a2b71d6993dd5f887030
Maria Pugacheva <marie.pugacheva@gmail.com>
2024-08-29 13:49:34 -0700

    Add a solution for #33

--- Readme.md
+++ Readme.md
@@ -19,6 +19,7 @@ LeetCode
 |26|[Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/)|[Python](src/python/_01_easy/_26_remove-duplicates-from-sorted-array.py)|Easy|1|N|N|
 |27|[Remove Element](https://leetcode.com/problems/remove-element/description/)|[Python](src/python/_01_easy/_27_remove-element.py)|Easy|2|Y|N|
 |28|[Find the Index of the First Occurrence in a String](https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/)|[Python](src/python/_01_easy/_28_find-the-index-of-the-first-occurrence-in-a-string.py)|Easy|1|YY|
+|33|[Search in Rotated Sorted Array](https://leetcode.com/problems/search-in-rotated-sorted-array/)|[Python](src/python/_02_medium/_33_search-in-rotated-sorted-array.py)|Medium|1|Y|N|
 |35|[Search Insert Position](https://leetcode.com/problems/search-insert-position/)|[Python](src/python/_01_easy/_35_search-insert-position.py)|Easy|2|N|
 |36|[Valid Sudoku](https://leetcode.com/problems/valid-sudoku/)|[Python](src/python/_02_medium/_36_valid-sudoku.py)|Medium|1|Y|Y|
 |45|[Jump Game II](https://leetcode.com/problems/jump-game-ii/)|[Python](src/python/_02_medium/_45_jump-game-II.py)|Medium|1|YYY|
---
+++ src/python/_02_medium/_33_search-in-rotated-sorted-array.py
@@ -0,0 +1,46 @@
+import doctest
+from typing import List
+
+
+# ---------------------------------------------------------------------
+# Approach 1: Binary Search. Time: O(log n)                         ***
+# ---------------------------------------------------------------------
+def solution(nums: List[int], t: int) -> int:
+    """Given an array nums sorted in ascending order (with distinct
+    values) and possibly rotated, and an integer t, return the index of
+    t if it is in nums; otherwise, return -1 if it is not in nums.
+
+    Examples:
+        >>> solution([1, 2], 1)
+        0
+        >>> solution([2, 1], 1)
+        1
+        >>> solution([4, 5, 6, 7, 0, 1, 2], 0)
+        4
+        >>> solution([4, 5, 6, 7, 0, 1, 2], 3)
+        -1
+        >>> solution([1], 0)
+        -1
+        >>> solution([3, 5, 1], 3)
+        0
+    """
+    i, j = 0, len(nums) - 1
+    while i <= j:
+        m = (i + j) // 2
+        if nums[m] == t:
+            return m
+        elif nums[0] <= nums[m]:
+            if nums[0] <= t < nums[m]:
+                j = m - 1
+            else:
+                i = m + 1
+        else:
+            if nums[m] < t <= nums[-1]:
+                i = m + 1
+            else:
+                j = m - 1
+    return -1
+
+
+if __name__ == '__main__':
+    doctest.testmod()
