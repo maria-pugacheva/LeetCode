704256dd9c3cd6025c59d257eac059faccb55da6
Maria Pugacheva <marie.pugacheva@gmail.com>
2025-05-08 15:54:22 -0700

    Add a solution for #408

---
+++ src/python/_01_easy/_408_valid-word-abbreviation.py
@@ -0,0 +1,45 @@
+import doctest
+
+
+# ---------------------------------------------------------------------
+# Approach 1: Two Pointers. Time: O(n + m). Space: O(1)             **!
+# ---------------------------------------------------------------------
+def solution_one(word: str, abbr: str) -> bool:
+    """Given a string word and an abbreviation abbr, return whether the
+    string matches the given abbreviation. A string can be abbreviated
+    by replacing any number of non-adjacent, non-empty substrings with
+    their lengths. The lengths should not have leading zeros.
+
+    Examples:
+        >>> solution_one('apple', 'a2e')
+        False
+        >>> solution_one('substitution', 's55n')
+        False
+        >>> solution_one('substitution', 's010n')
+        False
+        >>> solution_one('substitution', 's0ubstitution')
+        False
+        >>> solution_one('internationalization', 'i12iz4n')
+        True
+    """
+    n1, n2 = len(word), len(abbr)
+    i, j = 0, 0
+    while j < n2:
+        if abbr[j] == '0':
+            return False
+        if abbr[j].isnumeric():
+            n = 0
+            while j < n2 and abbr[j].isnumeric():
+                n = n * 10 + int(abbr[j])
+                j += 1
+            i += n
+        else:
+            if i < n1 and word[i] != abbr[j]:
+                return False
+            i += 1
+            j += 1
+    return i == n1 and j == n2
+
+
+if __name__ == '__main__':
+    doctest.testmod()
