f9da64c4dfca9a2e08ecadc0f86672b7cb675b53
Maria Pugacheva <marie.pugacheva@gmail.com>
2025-05-17 16:03:59 -0700

    Update #1539

--- src/python/_01_easy/_1539_kth-missing-positive-number.py
+++ src/python/_01_easy/_1539_kth-missing-positive-number.py
@@ -3,19 +3,54 @@ from typing import List
 
 
 # ---------------------------------------------------------------------
-# Approach 1: Binary Search. Time: O(log n). Space: O(1)              !
+# Approach 1: Brute Force. Time: O(n). Space: O(1)                    !
 # ---------------------------------------------------------------------
-# Note:
+# Note: For each element num in the array, if it is less than or equal
+#       to k, it means we've encountered an actual element of the
+#       sequence, so the k-th missing positive integer is pushed further
+#       by one. If num is greater than k, we stop because there’s no
+#       need to continue iterating once we’ve passed the range where the
+#       k-th missing integer could exist.
 # ---------------------------------------------------------------------
-def solution(nums: List[int], k: int) -> int:
+def solution_one(nums: List[int], k: int) -> int:
     """Given an array nums of positive integers sorted in a strictly
     increasing order, and an integer k. Return the k-th positive integer
     that is missing from this array.
 
     Examples:
-        >>> solution([1, 2, 3, 4], 2)
+        >>> solution_one([1], 1)
+        2
+        >>> solution_one([3], 1)
+        1
+        >>> solution_one([1, 2, 3, 4], 2)
         6
-        >>> solution([2, 3, 4, 7, 11], 5)
+        >>> solution_one([2, 3, 4, 7, 11], 5)
+        9
+    """
+    for n in nums:
+        if n <= k:
+            k += 1
+        else:
+            break
+    return k
+
+
+# ---------------------------------------------------------------------
+# Approach 2: Binary Search. Time: O(log n). Space: O(1)              !
+# ---------------------------------------------------------------------
+def solution_two(nums: List[int], k: int) -> int:
+    """Given an array nums of positive integers sorted in a strictly
+    increasing order, and an integer k. Return the k-th positive integer
+    that is missing from this array.
+
+    Examples:
+        >>> solution_two([1], 1)
+        2
+        >>> solution_two([3], 1)
+        1
+        >>> solution_two([1, 2, 3, 4], 2)
+        6
+        >>> solution_two([2, 3, 4, 7, 11], 5)
         9
     """
     i, j = 0, len(nums) - 1
